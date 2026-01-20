808c31cc7b48ae72ffec5b89386f2b9683fa17a6
namluong <luongkhacnam222@gmail.com>
2026-01-20 22:04:48 +0700

    update ui

--- src/renderer/src/components/HandDetect.tsx
+++ src/renderer/src/components/HandDetect.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
 import styled from 'styled-components'
-import { HandDetection } from '@renderer/AI/HandDetection'
+import { motion } from 'motion/react'
 
 type Props = {
   onSubmit(): void
@@ -29,7 +29,47 @@ export const HandDetect = ({ onSubmit }: Props): React.JSX.Element => {
       {/*Logo */}
       <Logo src={logo} alt="logo" />
       {/*<HandCamera />*/}
-      <HandDetection onClosen={onSubmit} />
+      <div
+        style={{
+          flex: 1,
+          width: '100%'
+        }}
+      >
+        Content
+      </div>
+
+      <Bottom>
+        <BtnMotion
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          type="button"
+          onClick={onSubmit}
+          style={{ backgroundColor: 'var(--yellow-color)' }}
+        >
+          No hand detected!
+        </BtnMotion>
+      </Bottom>
+      {/*<HandDetection onClosen={onSubmit} />*/}
     </Container>
   )
 }
+
+const Bottom = styled.div`
+  padding: 2rem;
+  background: var(--footer-gradient);
+  width: 100%;
+  text-align: center;
+`
+
+const StyledButton = styled.button`
+  font-weight: bold;
+  font-size: var(--text-lg);
+  padding: 1rem 4rem;
+  background-color: var(--yellow-color);
+  color: var(--button-text-color);
+  box-shadow: 0px 2px 100px 0px rgba(240, 209, 53, 1);
+  border-radius: 4rem;
+  max-width: 80vw;
+`
+
+const BtnMotion = motion(StyledButton)
--- src/renderer/src/components/Result.tsx
+++ src/renderer/src/components/Result.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../assets/vertical_logo.svg'
 import styled from 'styled-components'
+import handGuide from '../assets/hand-guide.gif'
 
 type Props = {
   tryAgain(): void
@@ -10,7 +11,15 @@ export const Result = ({ tryAgain }: Props): React.JSX.Element => {
   return (
     <Container>
       <img src={logo} alt="logo" />
-      <h1>Assessment results</h1>
+      <h1 style={{ width: '100%' }}>Assessment results</h1>
+
+      <Content>
+        <Answers>
+          <p>Thank you for participating in our assessment. Please review your results below:</p>
+        </Answers>
+
+        <img src={handGuide} alt="result" />
+      </Content>
       <button type="button" onClick={tryAgain}>
         try again
       </button>
@@ -24,4 +33,18 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 1rem;
+`
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 1rem;
+`
+const Answers = styled.div`
+  flex: 1;
+
+  p {
+    border: 2px solid var(--yellow-color);
+    padding: 1rem;
+  }
 `
